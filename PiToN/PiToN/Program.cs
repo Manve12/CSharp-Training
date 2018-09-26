f9f120186d381afe23a9a8b3514e734d3dd400d2
manvidas.barvicius <manvidas.barvicius@NM-16010-PC>
2018-09-26 14:11:40 +0100

    Added static PiToN method

---
+++ PiToN/PiToN/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PiToN
+{
+    class Program
+    {
+        /// <summary>
+        /// Rounds pi to a number of decimal places
+        /// </summary>
+        /// <param name="numberOfDecimalPlaces"></param>
+        /// <returns>Pi to a set number of decimal places (maximum 15)</returns>
+        public static double PiToDecimalPlaces(int numberOfDecimalPlaces)
+        {
+           return Math.Round(Math.PI, numberOfDecimalPlaces);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PiToDecimalPlaces(15));
+        }
+    }
+}
