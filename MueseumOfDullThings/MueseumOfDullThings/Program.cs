566638248134a1d9a009d52a68bcaa45fa2e7aa1
manvidas.barvicius <manvidas.barvicius@NM-16010-PC>
2018-09-26 13:45:46 +0100

    Added a RemoveLowestRating method

---
+++ MueseumOfDullThings/MueseumOfDullThings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MueseumOfDullThings
+{
+    class Program
+    {
+        /// <summary>
+        /// Remove the lowest integer in an array
+        /// </summary>
+        /// <param name="ratingArray">Integer array to iterate over</param>
+        /// <returns>Array of integers without lowest or empty array</returns>
+        public static int[] RemoveLowestRating(int[] ratingArray)
+        {
+            try
+            {
+                var index = Array.IndexOf(ratingArray, ratingArray.Min());
+                var listRating = new List<int>(ratingArray);
+
+                listRating.RemoveAt(index);
+
+                return listRating.ToArray();
+            }
+            catch
+            {
+                return ratingArray;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var array = RemoveLowestRating(new int[] { 1, 2, 3, 4, 5 });
+            foreach(var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
